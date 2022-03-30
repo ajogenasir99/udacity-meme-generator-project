5479e16bca28f725c93ab8165004549c50bd71ff
Nas <nazylamar9@gmail.com>
2022-03-30 23:15:14 +0100

    wrote code to implement meme engine

---
+++ MemeEngine/MemeEngine.py
@@ -0,0 +1,35 @@
+from PIL import Image, ImageDraw, ImageFont
+import os
+import random
+
+
+class MemeEngine():
+    def __init__(self, output_dir):
+        self.output_dir = output_dir
+
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+    def make_meme(self, img_path, text, author, width=500) -> str:
+        img = Image.open(img_path)
+        outfile = os.path.join(
+            self.output_dir,
+            f"temp-{random.randint(0,10000000)}.jpg"
+        )
+        img_width, img_height = img.size
+        if img.width > width:
+            img_width, img_height = img.size
+            ratio = width/float(img_width)
+            img_height = int(img_height * ratio)
+            new_size = img.resize((width, img_height))
+        else:
+            new_size = img
+
+        d = ImageDraw.Draw(new_size)
+        fnt = ImageFont.truetype("./_data/Fonts/LilitaOne-Regular.ttf", 20)
+        text_pos = random.randint(35, img_height - 50)
+
+        d.text((10, text_pos), text, font=fnt, fill='white')
+        d.text((20, text_pos + 20), f"- {author}", font=fnt, fill="white")
+        new_size.save(outfile, "JPEG")
+        return outfile
