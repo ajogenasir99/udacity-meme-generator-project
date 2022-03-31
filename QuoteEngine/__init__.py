20ad75f41c7fb7967358c7c5d94599e332cd4fec
Nas <nazylamar9@gmail.com>
2022-03-31 17:19:34 +0100

    imported classes to init file for QuoteEngine

---
+++ QuoteEngine/__init__.py
@@ -0,0 +1,10 @@
+"""imports classes and makes them available 
+at module level"""
+
+from .DocXIngestor import DocXIngestor
+from .CsvIngestor import CsvIngestor
+from .PdfIngestor import PDFIngestor
+from .Ingestor import Ingestor
+from .QuoteModel import QuoteModel
+from .IngestorInterface import IngestorInterface
+from .TxtIngestor import TxtIngestor
